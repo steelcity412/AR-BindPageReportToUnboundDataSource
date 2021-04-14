154a0331e7d55b2a122df1e887757f4a88398cc6
ewarren1994 <37820139+steelcity412@users.noreply.github.com>
2021-04-14 10:16:40 -0400

    Initial Commit

---
+++ DataLayer.vb
@@ -0,0 +1,34 @@
+﻿Imports GrapeCity.ActiveReports.Expressions.ExpressionObjectModel
+Imports System.Globalization
+Imports System.Data.OleDb
+
+Friend NotInheritable Class DataLayer
+    Private _datasetData As System.Data.DataSet
+
+    Public Sub New()
+        LoadDataToDataSet()
+    End Sub
+
+    Public ReadOnly Property DataSetData() As System.Data.DataSet
+        Get
+            Return _datasetData
+        End Get
+    End Property
+
+    Private Sub LoadDataToDataSet()
+
+        Dim connStr As String = "Provider=Microsoft.Jet.OLEDB.4.0;Persist Security Info=False;
+                Data Source=" + Application.StartupPath + "\..\..\Data\NWIND.mdb"
+        Dim productSql As String = "SELECT top 15 * FROM Products"
+
+        _datasetData = New DataSet()
+        Dim conn As New OleDbConnection(connStr)
+        Dim cmd As OleDbCommand = Nothing
+        Dim adapter As New OleDbDataAdapter
+
+        cmd = New OleDbCommand(productSql, conn)
+        adapter.SelectCommand = cmd
+        adapter.Fill(_datasetData, "DataSet1")
+    End Sub
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Dim WithEvents runtime As GrapeCity.ActiveReports.Document.PageDocument
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        LoadReport()
+
+    End Sub
+
+    Private Sub LoadReport()
+        Dim rptPath As New System.IO.FileInfo("..\..\PageReport1.rdlx")
+        'Create a report definition that loads an existing report.
+        Dim definition As New GrapeCity.ActiveReports.PageReport(rptPath)
+        'Load the report definition into a new page document.
+        runtime = New GrapeCity.ActiveReports.Document.PageDocument(definition)
+        'Attach the runtime to an event. This line of code creates the event shell below.
+        Viewer1.LoadDocument(runtime)
+    End Sub
+
+    'ActiveReports raises this event when it cannot locate a report's data source in the usual ways.
+    Private Sub runtime_LocateDataSource(ByVal sender As Object, ByVal args As GrapeCity.ActiveReports.LocateDataSourceEventArgs) Handles runtime.LocateDataSource
+        Dim dl = New DataLayer
+        args.Data = dl.DataSetData.Tables("DataSet1")
+    End Sub
+
+End Class
